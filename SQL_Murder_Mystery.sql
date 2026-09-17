aec42adb6ea1f1e153f81270f3f49f81570765a4
Laichi Chanez <laichi.x.chanez.ui.ux@gmail.com>
2026-09-17 15:12:58 +0100

    Check gym members

--- SQL_Murder_Mystery.sql
+++ SQL_Murder_Mystery.sql
@@ -10,6 +10,7 @@ WHERE
 
 
 -- Getting the witnesses ID
+	-- using defined variables instead of subqueries for readability purpose
 
 -- 01. getting the northwestern Dr Witness
 WITH Northwestern_houses AS (
@@ -18,7 +19,7 @@ WITH Northwestern_houses AS (
 								WHERE 	address_street_name LIKE 'Northwestern Dr'
 )
 
-SELECT id INTO @witness_north
+SELECT id 
 FROM Northwestern_houses
 WHERE address_number = 	(
 							SELECT MAX(address_number)
@@ -33,20 +34,41 @@ FROM person
 WHERE address_street_name LIKE 'Franklin Ave' 
 AND name LIKE '%Annabel%'
 ;
-
-
-
--- 03. Combining those id's
-
-
-SELECT * 
-FROM interview
-INNER JOIN 
 ;
 
 
 
+-- 03. Combining those id's
+SELECT * 
+FROM interview
+WHERE person_id IN (@witness_north, @witness_franklin)
+;
 
+
+-- Sus goes to 'Get Fit Now Gym' & was there on January 9th 2018
+-- He is Gold member 
+-- Bag stated with 48Z
+-- Car plate include 'H42W'
+
+
+-- -- Check Gold members that where there in the 09th January
+WITH gold_members_48Z AS (
+							SELECT * 
+							FROM get_fit_now_member
+							WHERE	membership_status = 'gold'
+							AND 	id LIKE '48Z%'
+)
+-- Check Gold members that where there in the 09th January
+SELECT * 
+FROM get_fit_now_check_in
+where check_in_date = 20180109
+AND membership_id IN (
+						SELECT id
+						FROM get_fit_now_member
+						WHERE	membership_status = 'gold'
+						AND 	id LIKE '48Z%'
+)
+;
 
 
 
